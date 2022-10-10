d311e5f379b61d9777a454741726c75084a0e29e
Colin Jaffe <balloonasaurus@gmail.com>
2022-10-10 11:01:09 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,66 @@
+const prompt = require("prompt");
+const colors = require('@colors/colors/safe')
+
+const {questions, turtles} = require("./data");
+
+const responses = [];
+prompt.message = "";
+prompt.start();
+
+questions.forEach(({answers}) => {
+  for (let i = answers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = answers[i];
+    answers[i] = answers[j];
+    answers[j] = temp;
+  }
+})
+
+const ask = async (i) => {
+  if (i === questions.length) {
+    finishQuiz();
+
+    return;
+  }
+
+  const { text, answers } = questions[i];
+  const questionText =
+        "\n" +
+        colors.green(text) +
+        "\n\n" +
+        answers.map((answer, i) => `${colors.green(i + 1)}. ${answer.text}`).join("\n") +
+        "\n\n";
+
+  const question = {
+    name: "text",
+    message: questionText,
+  };
+
+  responses.push((await prompt.get(question)).text);
+  ask(i + 1);
+};
+
+ask(0);
+
+const finishQuiz = () => {
+  let currentTurtle;
+  let max = 0;
+  const scores = {}
+  responses.forEach((response, i) => {
+    const {turtle} = questions[i].answers.find((_, i) => Number(response.trim()) === i + 1)
+    if (turtle.id in scores) {
+      scores[turtle.id]++;
+    } else {
+      scores[turtle.id] = 1;
+    }
+
+    if (scores[turtle.id] > max) {
+      currentTurtle = turtle;
+      max = scores[turtle.id];
+    }
+
+    console.log(currentTurtle.displayName);
+  })
+
+  console.log("\n" + `Your turtle is: ${colors[currentTurtle.color](currentTurtle.displayName)}`);
+};
